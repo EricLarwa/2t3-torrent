ff8725a4ac4dc40da524c45a23f2619e5d90a224
Eric A. Larwa <Eric.ALarwa@airboss.com>
2025-08-01 13:26:33 -0400

    restructure and add download.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import fs from 'fs';
-import bencode from  'bencode'
-
+import download from './src/download.js';
 import getPeers from './src/tracker.js';
 import torrentParser from './src/torrent-parser.js';
 
-const torrent = torrentParser.open('puppy.torrent');
+const torrent = torrentParser.open(process.argv[2]);
+
+download(torrent)
 
 getPeers(torrent, (peers) => {
     console.log('Peers:', peers);
