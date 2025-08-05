c41aa8925bf837ae4a622f56db162d61818be310
EricLarwa <larwae22@students.ecu.edu>
2025-08-05 09:11:02 -0400

    add pieces'

---
+++ src/pieces.js
@@ -0,0 +1,27 @@
+
+class Pieces {
+    constructor(size) {
+        this.requested = new Array(size).fill(false)
+        this.received = new Array(size).fill(false)
+    }
+
+    addRequested(pieceIndex) {
+        this.requested[pieceIndex] = true
+    }
+
+    addReceived(pieceIndex) {
+        this.received[pieceIndex] = true
+    }
+
+    needed(pieceIndex) {
+        if(this.requested.every(i => i === true)) {
+            this.requested = this.requested.slice()
+        }
+        return !this.requested[pieceIndex]
+    }
+
+    IsDone() {
+        return this.received.every(i => i === true);
+    }
+
+}
